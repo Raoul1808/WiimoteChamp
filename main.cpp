7a660ea233abdf968997cbbc0c478b543032a607
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-10-15 22:08:00 +0200

    SDL2 + OpenGL 4.6 Window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
-#include <iostream>
+#include "SDL.h"
+#include "glad/glad.h"
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello, World!" << std::endl;
+    SDL_Init(SDL_INIT_VIDEO);
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 6);
+    SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
+
+    SDL_Window* window = SDL_CreateWindow("Wiimote Champ Debug GUI", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, SDL_WINDOW_OPENGL);
+
+    SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+    if (!gladLoadGLLoader(SDL_GL_GetProcAddress))
+    {
+        return -1;
+    }
+
+    SDL_GL_SetSwapInterval(1);
+
+    bool running = true;
+    SDL_Event ev;
+    while (running)
+    {
+        while (SDL_PollEvent(&ev))
+        {
+            if (ev.type == SDL_QUIT)
+            {
+                running = false;
+                break;
+            }
+        }
+
+        glClearColor(0.390625f, 0.58203125f, 0.92578125f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_GL_DeleteContext(glContext);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
