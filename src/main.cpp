af62141a92d751521e7b8cb538a83d7ad983fc56
Raoul1808 <37770353+Raoul1808@users.noreply.github.com>
2022-10-15 23:49:30 +0200

    Add WiiUse from custom fork

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,38 @@
 #include "SDL.h"
 #include "window.hpp"
 #include "imgui.h"
+#include "wiiuse.h"
 
 int main(int argc, char** argv)
 {
     Window window{};
 
+    wiimote** remotes;
+
+    remotes = wiiuse_init(1);
+
+    bool remoteFound = false;
+    bool wiimoteConnected = false;
+
     while (window.IsRunning())
     {
         window.PollEvents();
+
         window.PreRender();
-        ImGui::ShowDemoWindow();
+        ImGui::Begin("Wiimote State");
+        if (ImGui::Button("Refresh Wiimote"))
+        {
+            remoteFound = wiiuse_find(remotes, 1, 5);
+            if (remoteFound)
+            {
+                wiimoteConnected = wiiuse_connect(remotes, 1);
+            }
+        }
+        if (remoteFound && wiimoteConnected)
+        {
+            ImGui::Text("Found a remote!");
+        }
+        ImGui::End();
         window.PostRender();
     }
 
